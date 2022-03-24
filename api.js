0ffcf21d8a3fa37f3e2ef2ddfd8e446cf85c9491
Steven Jocol <steven.jsg@gmail.com>
2022-03-23 21:11:38 -0600

    NodeStatus fix
    Final fix for the nodestatus translation, host and port is now passed in the params

--- api.js
+++ api.js
@@ -1,17 +1,16 @@
 const Net = require('net');
 const NodePort = 8080;
 const NodeHost = '192.210.226.118';
-const client = new Net.Socket();
 
 const getRoot = async (request, response) => {
     response.status(200).send("ok");
 }
 
 const getNodeStatus = async(request, response) => {
-
-    console.log("Request for NodeStatus started...");
-    client.connect({port: NodePort, host: NodeHost}, function() {
-        console.log("TCP connection established");
+    const {host, port} = request.query;
+    
+    const client = new Net.Socket();
+    client.connect({port: port, host: host}, function() {
         client.write("NODESTATUS\n");
     });
 
@@ -21,9 +20,6 @@ const getNodeStatus = async(request, response) => {
         return response.status(200).send(res);
     });
 }
-
-
-
 
 module.exports = {
     getRoot,
