18321598cf19e14e6ae0d7b5269463d0c9adbc3c
Stevengez <steven.jsg@gmail.com>
2022-04-10 18:18:15 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const api = require('./api');
-const localPort = process.env.PORT || 5000
+const localPort = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
